40a8a1ddd8a2093b94af3371ad498facdcfaa038
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2023-04-22 18:15:47 -0500

    Add files via upload

---
+++ src/app/dbz/components/pages/main-page/main-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from 'src/app/dbz/components/interfaces/dbz.interfaces';
+import { DbzService } from '../../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  // personajes: Personaje [] = [];
+
+  nuevo: Personaje = {
+    nombre: ' Gohan',
+    poder: 600
+  };
+
+  // get personajes():Personaje[]{
+  //   return this.dbzService.personajes;
+  // }
+
+  // agregarNuevoPersonaje(argumento: Personaje){
+  //   this.personajes.push(argumento);
+  // }
+
+  //  constructor(private dbzService: DbzService){
+  //    this.personajes = this.dbzService.personajes;
+  //  }
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MainPageComponent } from './main-page/main-page.component';
-import { PersonajesComponent } from './personajes/personajes.component';
-import { AgregarComponent } from './agregar/agregar.component';
-import { DbzService } from './services/dbz.service';
+import { MainPageComponent } from 'src/app/dbz/components/pages/main-page/main-page.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
+import { DbzService } from 'src/app/dbz/components/services/dbz.service';
 
 
 
